3ad5531c102699b01ecbb6224cfd8e8dac1e78b0
ReversiblWizard <Sayakmitra16@gmail.com>
2025-05-17 23:20:05 +0530

    17-05-2025 v2

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -10,7 +10,24 @@ for (var i = 0; i< numberOfDrumButtons; i++){
         // console.log(buttonInnerHtml);
         // console.log(this);
 
-        switch (buttonInnerHtml) {
+        makeSound(buttonInnerHtml);
+
+        buttonAnimation(buttonInnerHtml);
+       
+});
+
+document.addEventListener("keypress", function(event) {
+    // alert("Key was pressed");
+    // console.log(event);
+
+
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+     switch (key) {
             case "w":
                 var tom1 = new Audio("sounds/tom-1.mp3");
                 tom1.play();
@@ -50,5 +67,14 @@ for (var i = 0; i< numberOfDrumButtons; i++){
                 console.log();
         }
 
-    });
+    };
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
